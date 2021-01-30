e888e662ffe38c2f722ebae96571feccc78279db
wisesm0 <dyadicofficial@gmail.com>
2021-01-30 14:42:21 -0800

    getting started

--- Assets/Scripts/Game/AsteroidsGameManager.cs
+++ Assets/Scripts/Game/AsteroidsGameManager.cs
@@ -117,7 +117,7 @@ namespace Photon.Pun.Demo.Asteroids
 
         public override void OnDisconnected(DisconnectCause cause)
         {
-            UnityEngine.SceneManagement.SceneManager.LoadScene("DemoAsteroids-LobbyScene");
+            UnityEngine.SceneManagement.SceneManager.LoadScene(SceneNames.LOBBY);
         }
 
         public override void OnLeftRoom()
---
+++ Assets/Scripts/SceneNames.cs
@@ -0,0 +1,5 @@
+
+public class SceneNames {
+    public static readonly string SCENE = "GameScene";
+    public static readonly string LOBBY = "LobbyScene";
+}
